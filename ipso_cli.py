f044a37eca055504de67fdf8e3ee4a820864a958
Felicià Maviane Macia <felicia.maviane-macia@inra.fr>
2020-07-23 14:44:12 +0200

    Sem-Ver: bugfix - Better command line handling - Removed main as an entry point

--- ipso_cli.py
+++ ipso_cli.py
@@ -1,69 +1,102 @@
 import sys
 import argparse
 import logging
+import os
+import logging
+
+if not os.path.exists("logs"):
+    os.mkdir("logs")
+logging.basicConfig(
+    filename=os.path.join("logs", "log.log"),
+    filemode="a",
+    level=logging.INFO,
+    format="[%(asctime)s - %(name)s - %(levelname)s] - %(message)s",
+)
+logger = logging.getLogger("IPSO CLI entry unit")
+logger.info("_________________________________________________________________________")
+logger.info("Launching IPSO CLI as a module...")
 
 from ipapi.base.pipeline_launcher import launch
 
 
 def exec_cli():
 
-    parser = argparse.ArgumentParser(description="Run a pipeline on target")
+    parser = argparse.ArgumentParser(
+        description="Command line interface for pipelines built with IPSO Phen",
+        epilog="""
+        Even if all arguments are optional, some must be present.
+        Stored state can be used alone. If not present there must 
+        be a source a script an output_folder and a csv_file_name at least.
+        """,
+        allow_abbrev=False,
+    )
     parser.add_argument(
-        "-s",
-        "--stored_state",
+        "--stored-state",
         default=None,
         required=False,
         help="Path to the stored state built with IPSO Phen",
+        dest="stored_state",
     )
     parser.add_argument(
-        "-p",
         "--script",
         required=False,
         help="File containing an IPSO Phen pipeline",
         default=None,
+        dest="script",
     )
     parser.add_argument(
-        "-i", "--image", required=False, help="Image to be processed", default=None
+        "--image", required=False, help="Image to be processed", default=None, dest="image"
     )
     parser.add_argument(
-        "-l",
-        "--image_list",
+        "--image-list",
         required=False,
         help="File containing a list of images to be processed",
         default=None,
+        dest="image_list",
     )
     parser.add_argument(
-        "-c",
-        "--thread_count",
+        "--thread-count",
         required=False,
         help="Override number of concurrent processes",
         default=None,
+        type=int,
+        dest="thread_count",
     )
     parser.add_argument(
-        "-o",
-        "--output_folder",
+        "--output-folder",
         required=False,
         help="CSV and data output folder",
         default=None,
+        dest="output_folder",
     )
     parser.add_argument(
-        "-f", "--csv_file_name", required=False, help="Merged CSV file name", default=None
+        "--csv-file-name",
+        required=False,
+        help="Merged CSV file name",
+        default=None,
+        dest="csv_file_name",
     )
     parser.add_argument(
-        "-w",
         "--overwrite",
         required=False,
         help="Overwrite existing partial files",
-        default=False,
+        action="store_true",
+        dest="overwrite",
     )
 
     args = vars(parser.parse_args())
-    logger = logging.getLogger(__name__)
+    logger.info("Retrieved parameters")
     for k, v in args.items():
-        logger.info(f"{k}: {v}")
+        logger.info(f"  * {k}: {v}")
 
     return launch(**args)
 
 
 if __name__ == "__main__":
-    sys.exit(exec_cli())
+    res = exec_cli()
+    if res == 0:
+        logger.info("Closing IPSO CLI")
+    else:
+        logger.error("Closing IPSO CLI")
+    logger.info("_________________________________________________________________________")
+    sys.exit(res)
